3561eaef5db433350893afcc2c9bf53a134532da
Noah Santos <111617611+Noah-Santos@users.noreply.github.com>
2024-01-26 16:01:21 -0700

    Started Styling
    Started styling the form section

--- src/App.jsx
+++ src/App.jsx
@@ -11,27 +11,27 @@ function App() {
   const [sort, setSort] = useState(false);
   const [panel, setPanel] = useState(false);
   const [categoryForm, setCategoryForm] = useState(true)
-  // console.log(task)
 
   return (
     <div className='bodySection'>
       <div className="content">
         {/* opens the panel where the user can use the task and category forms */}
-        {!panel ? <FaCirclePlus onClick={()=>setPanel(true)}/> : 
-          <div>
-            <IoCloseCircle onClick={()=>setPanel(false)}></IoCloseCircle>
-            <div className="decideBtn">
-              <button onClick={()=>setCategoryForm(true)}>Categories</button>
-              <button onClick={()=>setCategoryForm(false)}>Tasks</button>
+        {!panel ? <FaCirclePlus onClick={()=>setPanel(true)} className='openBtn'/> : 
+          <>
+            <IoCloseCircle onClick={()=>setPanel(false)} className='closeBtn'></IoCloseCircle>
+            <div className='formSection'>
+              <div className="decideBtn">
+                <button onClick={()=>setCategoryForm(true)}>Categories</button>
+                <button onClick={()=>setCategoryForm(false)}>Tasks</button>
+              </div>
+              {/* determines which form should show */}
+              {categoryForm ? <Category categors={category}/> : <Task tasks={task}/>}
             </div>
-            {/* determines which form should show */}
-            {categoryForm ? <Category categors={category}/> : <Task tasks={task}/>}
-          </div>
+          </>
         }
 
         <button onClick={()=>setSort(true)}>Sort by Category</button>
         <button onClick={()=>setSort(false)}>Unsort</button>
-        {/* <Item tasks={task} categors={category} type={sort}></Item> */}
         {/* determines whether or not to sort the tasks by category */}
         {sort ? 
           <div>
@@ -41,11 +41,6 @@ function App() {
             <Item tasks={task} categors={category} type={false}></Item>
           </div>
         }
-        {/* {task.map((t, i)=>{
-          return (
-            <Item key={i} tasks={t}></Item>
-          )
-        })} */}
       </div>
     </div>
   );
